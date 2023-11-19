36bf8f363ca1fd3187d98f913c787a636d3f2861
Adithya S K <adithyaskolavi@gmail.com>
2023-11-20 02:18:28 +0530

    Added metadata files and update event log - Adithya S K

--- raft/node.py
+++ raft/node.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, BackgroundTasks
 import json
 import random
@@ -18,6 +19,17 @@ class Node:
         self.heartbeat_interval = 2
         self.current_term = 0
         self.config = self.read_config("config.json")
+        self.create_node_files()
+
+    def create_node_files(self):
+        os.makedirs(str(self.port), exist_ok=True)
+        metadata_template = self.read_metadata_template()
+        self.create_or_update_file(f"{self.port}/metadata.json", metadata_template)
+        self.create_or_update_file(f"{self.port}/eventlog.json", [])
+
+    def create_or_update_file(self, file_path, data):
+      with open(file_path, 'w') as file:
+          json.dump(data, file, indent=4)
 
     @staticmethod
     def read_config(file_path):
@@ -29,10 +41,37 @@ class Node:
         except json.JSONDecodeError:
             sys.exit("Configuration file is invalid.")
 
+    def read_metadata_template(self):
+        try:
+            with open("metajson_schema.json", "r") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print("Metadata schema file not found.")
+            return {}  # Return an empty dict or handle this case as needed
+        except json.JSONDecodeError:
+            print("Metadata schema file is invalid.")
+            return {}  # Return an empty dict or handle this case as needed
+
     @staticmethod
     def write_config(file_path, data):
         with open(file_path, "w") as file:
             json.dump(data, file, indent=4)
+            
+    def update_eventlog(self, action, port):
+        eventlog = self.read_file(f"{self.port}/eventlog.json")
+        eventlog.append({"timestamp": datetime.now().isoformat(), f"port_{action}": port})
+        self.create_or_update_file(f"{self.port}/eventlog.json", eventlog)
+
+    def read_file(self, file_path):
+        try:
+            with open(file_path, 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print(f"File not found: {file_path}")
+            return []  # or return a default value appropriate for your application
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON from file: {file_path}")
+            return []  # or return a default value
 
 class Leader(Node):
     def __init__(self, port):
@@ -40,9 +79,11 @@ class Leader(Node):
         threading.Thread(target=self.heartbeat_task, daemon=True).start()
 
     def send_heartbeat(self, follower_port):
+        metadata = self.read_file(f"{self.port}/metadata.json")
         try:
-            response = requests.get(f"http://localhost:{follower_port}/heartbeat")
+            response = requests.post(f"http://localhost:{follower_port}/heartbeat", json=metadata)
             print(f"Heartbeat acknowledged by follower on port {follower_port}: {response.json()}")
+            self.update_eventlog("sent", follower_port)
         except requests.RequestException:
             print(f"Failed to send heartbeat to follower on port {follower_port}")
 
@@ -105,11 +146,13 @@ async def register_follower(follower_data: FollowerRegistration, background_task
 
     return {"message": "Follower registered"}
 
-@app.get("/heartbeat")
-def heartbeat():
+@app.post("/heartbeat")
+def heartbeat(metadata: dict):
     if isinstance(node, Follower):
         with threading.Lock():
             node.last_heartbeat_time = datetime.now()
+            node.create_or_update_file(f"{node.port}/metadata.json", metadata)
+            node.update_eventlog("received", node.port)
     return {"message": "Acknowledged"}
 
 if __name__ == "__main__":
