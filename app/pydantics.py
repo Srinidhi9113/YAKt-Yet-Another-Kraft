b9194829ce9c21579e280db1baa119f57490c005
Achala-Nayak <106529093+Achala-Nayak@users.noreply.github.com>
2023-11-20 21:05:13 +0530

    Updated base.py- Achala Nayak

--- app/pydantics.py
+++ app/pydantics.py
@@ -1,27 +1,55 @@
 from typing import List
 from pydantic import BaseModel
 
-from pydantic import BaseModel
-from typing import List
-
-class BrokerRecord(BaseModel):
+class RegisterBrokerRecord(BaseModel):
+    internalUUID: str
     brokerId: int
-    brokerHost:str
-    brokerPort:int
-    securityProtocol:str
-    rackId:str
+    brokerHost: str
+    brokerPort: str
+    securityProtocol: str
+    brokerStatus: str
+    rackId: str
+    epoch: int
+
+class RegisterBrokerRecords(BaseModel):
+    records: List[RegisterBrokerRecord]
+    timestamp: str
 
 class TopicRecord(BaseModel):
-    name: str
+    records: List[dict]  # You can create a Pydantic model for this dictionary if needed
+    timestamp: str
 
 class PartitionRecord(BaseModel):
     partitionId: int
+    topicUUID: str
     replicas: List[str]
     ISR: List[str]
+    removingReplicas: List[str]
+    addingReplicas: List[str]
     leader: str
+    partitionEpoch: int
 
+class PartitionRecords(BaseModel):
+    records: List[PartitionRecord]
+    timestamp: str
 
 class ProducerIdsRecord(BaseModel):
     brokerId: str
     brokerEpoch: int
     producerId: int
+
+class ProducerIdsRecords(BaseModel):
+    records: List[ProducerIdsRecord]
+    timestamp: str
+
+class RegistrationChangeBrokerRecord(BaseModel):
+    brokerId: str
+    brokerHost: str
+    brokerPort: str
+    securityProtocol: str
+    brokerStatus: str
+    epoch: int
+
+class RegistrationChangeBrokerRecords(BaseModel):
+    records: List[RegistrationChangeBrokerRecord]
+    timestamp: str
