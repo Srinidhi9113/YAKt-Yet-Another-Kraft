a71091ef7b9565925aa1e9f153afc7b4bfab4d14
Achala-Nayak <106529093+Achala-Nayak@users.noreply.github.com>
2023-11-20 08:40:48 +0530

    Added pydantics.py file- Achala Nayak

--- app/base.py
+++ app/base.py
@@ -2,13 +2,18 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 import json
+from datetime import datetime
+import uuid
 
 app = FastAPI()
 
 # Define your Pydantic models here
 class BrokerRecord(BaseModel):
-    # Define the fields according to your JSON schema
-    pass
+    brokerId: int
+    brokerHost:str
+    brokerPort:int
+    securityProtocol:str
+    rackId:str
 
 class TopicRecord(BaseModel):
     # Define the fields
@@ -17,29 +22,80 @@ class TopicRecord(BaseModel):
 # ... Define other models
 
 # Utility functions to handle data storage and retrieval
-def load_data():
-    # Load data from JSON file
-    pass
+def load_data(path):
+    try:
+        with open(path, 'r') as file:
+            data = json.load(file)
+            return data
+    except:
+        data =  {
+            "RegisterBrokerRecords": {
+                "records": [],
+                "timestamp": ""
+            },
+            "TopicRecord": {
+                "records": [],
+                "timestamp": ""
+            },
+            "PartitionRecord": {
+                "records": [],
+                "timestamp": ""
+            },
+            "ProducerIdsRecord": {
+                "records": [],
+                "timestamp": ""
+            },
+            "RegistrationChangeBrokerRecord": {
+                "records": [],
+                "timestamp": ""
+            }
+        }
+        return data
 
-def save_data(data):
-    # Save data to JSON file
-    pass
+def save_data(path,data):
+    with open(path, 'w') as file:
+        json.dump(data, file, indent=2)
+
+def checkBrokerExists(brokerData,data):
+    found_dict = next((broker for broker in data if broker.get("brokerId") == brokerData['brokerId']), None)
+    return found_dict
+
 
 # CRUD API Endpoints
 @app.post("/register_broker/")
 async def register_broker(broker: BrokerRecord):
-    # Logic to register a broker
-    pass
+    filePath = "./metadata-8000.json"
+    data = load_data(filePath)
+    foundDict = checkBrokerExists(broker.dict(),data["RegisterBrokerRecords"]["records"])
+    if(foundDict):
+        return foundDict['internal_uuid']
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    data["RegisterBrokerRecords"]['timestamp'] = timestamp
+    serverSetup = broker.dict()
+    serverSetup["internal_uuid"] = str(uuid.uuid4())
+    serverSetup["brokerStatus"] = "ALIVE"
+    serverSetup["epoch"] = 0
+    data["RegisterBrokerRecords"]["records"].append(serverSetup)
+    save_data(filePath,data)
+    return serverSetup["internal_uuid"]
 
 @app.get("/brokers/")
-async def get_brokers():
-    # Logic to get all brokers
-    pass
+async def get_broker():
+    filePath = "./metadata-8000.json"
+    data = load_data(filePath)
+    return data["RegisterBrokerRecords"]["records"]
 
 @app.delete("/broker/{broker_id}")
 async def delete_broker(broker_id: int):
-    # Logic to delete a broker
-    pass
+    filePath = "./metadata-8000.json"
+    data = load_data(filePath)
+    brokers = data["RegisterBrokerRecords"]["records"]
+    index = next((index for index, broker in enumerate(brokers) if broker.get("brokerId") == broker_id), None)
+    if index is not None:
+        deletedBroker = brokers.pop(index)
+    data["RegisterBrokerRecords"]["records"] = brokers
+    save_data(filePath,data)
+    return deletedBroker
 
 # ... Define other CRUD endpoints
 
